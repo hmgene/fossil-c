2dfd6e7689ed8381397bd5dae3faed0936edd6b1
Hyunmin Kim <human.gim@gmail.com>
2023-02-07 15:05:52 -0500

    Create traim-fastq.sh

---
+++ traim-fastq.sh
@@ -0,0 +1,81 @@
+
+data=(
+trex_cells /mnt/rstor/SOM_GENE_BEG33/data/111122_NovoG/T_Rex_Cells_111122_NovoG_R1.fastq.gz
+bra_cells_mix /mnt/rstor/SOM_GENE_BEG33/users/hxk728/pj/fossil-c/01-init/fastq/BrachyCells_Mix_122121_AK_NovoG_R1.fastq.gz
+bra_cells_supe /mnt/rstor/SOM_GENE_BEG33/users/hxk728/pj/fossil-c/01-init/fastq/BrachyCells_Supe_122121_AK_NovoG_R1.fastq.gz
+bra_cells_und /mnt/rstor/SOM_GENE_BEG33/users/hxk728/pj/fossil-c/01-init/fastq/BrachyCells_UnD_122121_AK_NovoG_R1.fastq.gz
+bra_cells_cvcc_3p1 /mnt/rstor/SOM_GENE_BEG33/data/080122_NovoG/Brachy_CVCC_O_3p1_CKDL220018061-1A_H37NNDSX5_L1_1.fq.gz
+bra_cells_cvcc_3p3 /mnt/rstor/SOM_GENE_BEG33/data/080122_NovoG/Brachy_CVCC_O_3p3_CKDL220018061-1A_H37NNDSX5_L1_1.fq.gz
+bra_cells_kapa_3p1 /mnt/rstor/SOM_GENE_BEG33/data/080122_NovoG/BrachyCV_KAPA_3p1_CKDL220018061-1A_H37NNDSX5_L1_1.fq.gz
+bra_cells_kapa_3p3 /mnt/rstor/SOM_GENE_BEG33/data/080122_NovoG/BrachyCV_KAPA_3p3_CKDL220018061-1A_H37NNDSX5_L1_1.fq.gz
+bra_cells_kapa_3p5 /mnt/rstor/SOM_GENE_BEG33/data/080122_NovoG/BrachyCV_KAPA_3p5_CKDL220018061-1A_H37NNDSX5_L1_1.fq.gz
+bra_cells_cc_pip /mnt/rstor/SOM_GENE_BEG33/users/hxk728/dn/usftp21.novogene.com/01.RawData/BrachyC_CC_pip/BrachyC_CC_pip_CKDL220015104-1A_HY7MKDSX3_L3_1.fq.gz
+bra_cells_pl_pip /mnt/rstor/SOM_GENE_BEG33/users/hxk728/dn/usftp21.novogene.com/01.RawData/BrachyC_PL_pip/BrachyC_PL_pip_CKDL220015104-1A_HY7MKDSX3_L3_1.fq.gz
+bra_cells_cc /mnt/rstor/SOM_GENE_BEG33/data/022522_AK_NovoG/BrachyCells_ChromCapt_021422_NovoG_R1.fastq.gz
+bra_cells_pl /mnt/rstor/SOM_GENE_BEG33/data/022522_AK_NovoG/BrachyCells_ProxLig_021422_NovoG_R1.fastq.gz
+
+trex_vessels /mnt/rstor/SOM_GENE_BEG33/data/111122_NovoG/T_Rex_Vessels_111122_NovoG_R1.fastq.gz
+bra_vessels_mix /mnt/rstor/SOM_GENE_BEG33/users/hxk728/pj/fossil-c/01-init/fastq/BrachyVessels_Mix_122121_AK_NovoG_R1.fastq.gz
+bra_vessels_supe /mnt/rstor/SOM_GENE_BEG33/users/hxk728/pj/fossil-c/01-init/fastq/BrachyVessels_Supe_122121_AK_NovoG_R1.fastq.gz
+bra_vessels_und /mnt/rstor/SOM_GENE_BEG33/users/hxk728/pj/fossil-c/01-init/fastq/BrachyVessels_UnD_122121_AK_NovoG_R1.fastq.gz
+bra_vessels_cvcc_3p2 /mnt/rstor/SOM_GENE_BEG33/data/080122_NovoG/Brachy_CVCC_O_3p2_CKDL220018061-1A_H37NNDSX5_L1_1.fq.gz
+bra_vessels_cvcc_3p4 /mnt/rstor/SOM_GENE_BEG33/data/080122_NovoG/Brachy_CVCC_O_3p4_CKDL220018061-1A_H37NNDSX5_L1_1.fq.gz
+bra_vessels_kapa_3p2 /mnt/rstor/SOM_GENE_BEG33/data/080122_NovoG/BrachyCV_KAPA_3p2_CKDL220018061-1A_H37NNDSX5_L1_1.fq.gz
+bra_vessels_kapa_3p4 /mnt/rstor/SOM_GENE_BEG33/data/080122_NovoG/BrachyCV_KAPA_3p4_CKDL220018061-1A_H37NNDSX5_L1_1.fq.gz
+bra_vessels_kapa_3p6 /mnt/rstor/SOM_GENE_BEG33/data/080122_NovoG/BrachyCV_KAPA_3p6_CKDL220018061-1A_H37NNDSX5_L1_1.fq.gz
+bra_vessels_pl_pip /mnt/rstor/SOM_GENE_BEG33/users/hxk728/dn/usftp21.novogene.com/01.RawData/BrachyV_PL_pip/BrachyV_PL_pip_CKDL220015104-1A_HY7MKDSX3_L3_1.fq.gz
+bra_vessels_cc_pip /mnt/rstor/SOM_GENE_BEG33/users/hxk728/dn/usftp21.novogene.com/01.RawData/BrachyV_CC_pip/BrachyV_CC_pip_CKDL220015104-1A_HY7MKDSX3_L3_1.fq.gz
+bra_vessels_cc /mnt/rstor/SOM_GENE_BEG33/data/022522_AK_NovoG/BrachyVessels_ChromCapt_021422_NovoG_R1.fastq.gz
+bra_vessels_pl /mnt/rstor/SOM_GENE_BEG33/data/022522_AK_NovoG/BrachyVessels_ProxLig_021422_NovoG_R1.fastq.gz
+
+ost_cc /mnt/rstor/SOM_GENE_BEG33/users/hxk728/pj/fossil-c/01-init/fastq/Ostrich_ChromCapt_WG_081721_R1.fastq.gz
+ost_ep /mnt/rstor/SOM_GENE_BEG33/users/hxk728/pj/fossil-c/01-init/fastq/Ostrich_EndPolish_WG_081721_R1.fastq.gz
+ost_om /mnt/rstor/SOM_GENE_BEG33/users/hxk728/pj/fossil-c/01-init/fastq/Ostrich_OmniC_081721_R1.fastq.gz
+kcc14_dmso /mnt/rstor/SOM_GENE_BEG33/data/021522_CWRU/KCC14_DMSO_021522_CWRU_R1.fastq.gz
+srr1073105 /mnt/rstor/SOM_GENE_BEG33/users/hxk728/pj/fossil-c/05-merge-by-cv/fastq/SRR10173105_1.fastq.gz
+srr10173838 /mnt/rstor/SOM_GENE_BEG33/users/hxk728/pj/fossil-c/05-merge-by-cv/fastq/SRR10173838_1.fastq.gz
+err844243 /mnt/rstor/SOM_GENE_BEG33/users/hxk728/pj/fossil-c/05-merge-by-cv/fastq/ERR844243.fastq.gz
+err844310 /mnt/rstor/SOM_GENE_BEG33/users/hxk728/pj/fossil-c/05-merge-by-cv/fastq/ERR844310.fastq.gz
+rh4_hsalt_h3k27ac /mnt/rstor/SOM_GENE_BEG33/data/070622_NovoG/RH4_HSalt_H3K27ac_070622_NovoG_R1.fastq.gz
+cara_muscle /mnt/rstor/SOM_GENE_BEG33/users/hxk728/pj/fossil-c/06-trex/fq/SRR9849388_1.fastq.gz
+cara_bone /mnt/rstor/SOM_GENE_BEG33/users/hxk728/pj/fossil-c/06-trex/fq/SRR9849389_1.fastq.gz
+cara_fossil /mnt/rstor/SOM_GENE_BEG33/users/hxk728/pj/fossil-c/06-trex/fq/SRR9849390_1.fastq.gz
+)
+
+fastp=/home/hxk728/pr/fastp
+nproc=8
+mem=32g
+
+odir=fq1
+mkdir -p $odir
+for (( i=0;i<${#data[@]};i+=2 ));do
+        n=${data[$i]};
+        f1=${data[$i+1]};
+        f2=${f1/1\.f/2\.f};
+
+        for q in QLG QLg QLgx QLgxy Qgxy gxy;do
+                o=$odir/$q/$n
+                qq=` echo $q | perl -npe '$_=~s/(\w)/ -$1/g'`
+                echo $q $qq $o
+                mkdir -p ${o%\/*};
+
+                if [[  -f $f1 && -f $f2 ]];then
+                        cat<<-eof | sbatch -N 1 -n 1 -c $nproc --mem $mem
+                        #!/bin/bash -l
+                        $fastp -i $f1 -I $f2 -o ${o}_R1.fastq.gz -O ${o}_R2.fastq.gz -h $o.html -j $o.json --detect_adapter_for_pe -w $nproc $qq
+                        eof
+
+                #       $fastp -i $f1 -I $f2 -o ${o}_R1.fastq.gz -O ${o}_R2.fastq.gz \
+                #               -Q -x -y -g -l 10 -h $o.html -j $o.json --detect_adapter_for_pe -w $nproc \
+                #               --unpaired1 ${o}_unpaired_R1.fastq.gz --unpaired2 ${o}_unpaired_R2.fastq.gz
+                #       #       -m --merged_out ${o}_merged.fastq.gz
+
+                else
+                        cat<<-eof | sbatch -N 1 -n 1 -c $nproc --mem $mem
+                        #!/bin/bash -l
+                        $fastp -i $f1 -o ${o}_R1.fastq.gz -h $o.html -j $o.json -w $nproc $qq
+                        eof
+                fi
+        done
+done
+
