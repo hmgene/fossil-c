229152e5a8402709b8a9d84c2d9cf6488a6fa60f
Kim;Hyunmin <human.gim@gmail.com>
2023-02-23 13:06:12 -0500

    blt

--- src/fq.sh
+++ src/fq.sh
@@ -1,3 +1,12 @@
+
 fq2fa(){
-	cat $1 | perl -ne 'chomp;if( $. % 4 == 2){ print ">$.\n$_\n";}'
+	usage="$FUNCNAME <fq> [<prefix>]"
+	if [ $# -lt 2 ];then echo "$usage";return;fi
+	cat $1 | perl -ne 'chomp;if( $. % 4 == 2){ $i="'${2:-""}'".int($./4+1);print ">$i\n$_\n";}'
+}
+fq2fa-test(){
+echo "input:"
+echo -e "@id\nACGT\n+\nIIII\n@id\nACGT\n+\nIIII"
+echo "output:"
+echo -e "@id\nACGT\n+\nIIII\n@id\nACGT\n+\nIIII" | fq2fa - "pref"
 }
--- src/util.sh
+++ src/util.sh
@@ -3,3 +3,8 @@ BB=$BASEDIR/bin/`uname -sm | tr " " "_"`
 blat(){
 	$BB/$FUNCNAME $@;
 }
+w2rap-contigger(){
+	$BB/$FUNCNAME $@;
+/home/hxk728/git/fossil-c/tools/w2rap-contigger/bin/w2rap-contigger
+}
+
