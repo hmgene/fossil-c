19e084747fe722173e15394b7e42420c348a7539
Kim;Hyunmin <human.gim@gmail.com>
2023-03-07 12:10:59 -0500

    try

--- src/fq.sh
+++ src/fq.sh
@@ -10,3 +10,29 @@ echo -e "@id\nACGT\n+\nIIII\n@id\nACGT\n+\nIIII"
 echo "output:"
 echo -e "@id\nACGT\n+\nIIII\n@id\nACGT\n+\nIIII" | fq2fa - "pref"
 }
+
+fa-grep(){
+	usage="$FUNCNAME <fa> <name.list.file>"
+	local tmp=`mktemp -d`;
+	cat $2 > $tmp/a
+	cat $1 | perl -e 'use strict;
+		open(my $fh,"<","'$tmp/a'") or die "$!";
+		my %q=map {chomp; $_=>1 } <$fh>;
+		close($fh);
+
+		my $i="";
+		while(<STDIN>){chomp;
+			if($_=~/^>/){ $i=substr($_,1); 
+			}elsif( defined $q{$i} ){
+				print ">$i\n$_\n";
+			}	
+		}
+
+	'
+}
+fa-grep-test(){
+echo ">1
+AAAAA
+>2
+CCCC" | fa-grep - <( echo "1 2" | tr " " "\n" )
+}
