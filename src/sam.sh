47261e98429fb87385e486fe196e29a0d22a316c
Kim;Hyunmin <hxk728@hpc7.priv.cwru.edu>
2023-10-17 14:41:54 -0400

    dna-damage

--- src/sam.sh
+++ src/sam.sh
@@ -1,16 +1,22 @@
 sam2bed(){
-usage="$FUNCNAME <sam>"
+usage="$FUNCNAME <sam>
+ref: https://samtools.github.io/hts-specs/SAMv1.pdf
+"
 if [ $# -lt 1 ];then echo "$usage";return; fi
-        local n=${2:-${1##*/}};
-        cat $1 | perl -ne 'chomp;my@d=split/\t/,$_; my $n="'$n'";
-                sub cx{ my ($x)=@_;
-		## complexity
-                        my $y=0; map { $y += substr($x,$_,1) ne substr($x,$_ + 1,1) ? 1 : 0; } 0..(length($x)-2);
-                        return $y/length($x);
-                }
+        cat $1 | perl -e '
+	#@SQ     SN:JH731052     LN:57586
+	my %r=();
+	sub cx{ my ($x)=@_;
+	## complexity
+		my $y=0; map { $y += substr($x,$_,1) ne substr($x,$_ + 1,1) ? 1 : 0; } 0..(length($x)-2);
+		return $y/length($x);
+	}
+	while(<STDIN>){ chomp;my@d=split/\t/,$_; 
+		if( $_=~/SN:(\w+)\tLN:(\d+)/ ){
+			$r{ $1 } = $2; next;
+		}
                 my ($c,$s,$seq)=($d[2],$d[3]-1,$d[9]);
                 my $t = ($d[1] & 0x10) >  0 ? "-" : "+";
-                my $f = ($d[1] & 0x40) >  0 ? 1 : 0 ;
 
                 my $q=0; ## query pos
                 my $g=$s; ## genomic pos
@@ -18,11 +24,16 @@ if [ $# -lt 1 ];then echo "$usage";return; fi
                 while($d[5]=~/(\d+)([SNXMID])/g){
                         my ($x,$y)=($1,$2);
                         if($y=~/[SI]/){
+				next if( $r{$c} < $g+$x);
+
+				my $n=join(",", $d[0],"$d[5]::$x$y",$q,$q+$x,lc substr($seq,$q,$x),$t);
+				print join("\t",$c,$g,$g+$x,$n,0,$t),"\n";
                                 $q+=$x;
                         }elsif($y=~/[MX=]/){
-                                my $ss=substr($seq,$q,$x);
-                                my $cx1=cx($ss);
-                                print join("\t",$c,$g,$g+$x,"s:$ss,n:$n,f:$f",0,$t),"\n";
+				next if( $r{$c} < $g+$x);
+
+				my $n=join(",", $d[0],"$d[5]::$x$y",$q,$q+$x,uc substr($seq,$q,$x),$t);
+				print join("\t",$c,$g,$g+$x,$n,0,$t),"\n";
                                 $q+=$x; $g+=$x;
                         }elsif($y=~/[DN]/){
                                 $g+=$x;
@@ -30,6 +41,9 @@ if [ $# -lt 1 ];then echo "$usage";return; fi
                                 print {*STDERR} "warning $x$y\n";
                         }
                 }
+	}
         '
 }
+
+
 
