c09d4214676a16696960a023dd2b745a79251a0c
hmgene <human.gim@gmail.com>
2025-09-12 11:17:00 -0400

    1

--- 01-trim-fastq.sh
+++ 01-trim-fastq.sh
@@ -1,14 +1,14 @@
 input=(
-#/mnt/vstor/SOM_GENE_BEG33/data/082725_NovaSeq-X_A_lane2/Brachy_Blank_S9_L002_R1_001.fastq.gz
-#/mnt/vstor/SOM_GENE_BEG33/data/082725_NovaSeq-X_A_lane2/Brachy_cells_S5_L002_R1_001.fastq.gz
-#/mnt/vstor/SOM_GENE_BEG33/data/082725_NovaSeq-X_A_lane2/Brachy_c_sedi_S7_L002_R1_001.fastq.gz
-#/mnt/vstor/SOM_GENE_BEG33/data/082725_NovaSeq-X_A_lane2/Brachy_vessels_S6_L002_R1_001.fastq.gz
-#/mnt/vstor/SOM_GENE_BEG33/data/082725_NovaSeq-X_A_lane2/Brachy_v_sedi_S8_L002_R1_001.fastq.gz
+/mnt/vstor/SOM_GENE_BEG33/data/082725_NovaSeq-X_A_lane2/Brachy_Blank_S9_L002_R1_001.fastq.gz
+/mnt/vstor/SOM_GENE_BEG33/data/082725_NovaSeq-X_A_lane2/Brachy_cells_S5_L002_R1_001.fastq.gz
+/mnt/vstor/SOM_GENE_BEG33/data/082725_NovaSeq-X_A_lane2/Brachy_c_sedi_S7_L002_R1_001.fastq.gz
+/mnt/vstor/SOM_GENE_BEG33/data/082725_NovaSeq-X_A_lane2/Brachy_vessels_S6_L002_R1_001.fastq.gz
+/mnt/vstor/SOM_GENE_BEG33/data/082725_NovaSeq-X_A_lane2/Brachy_v_sedi_S8_L002_R1_001.fastq.gz
 /mnt/vstor/SOM_GENE_BEG33/data/082725_NovaSeq-X_A_lane2/Trex_cells_S10_L002_R1_001.fastq.gz
-#/mnt/vstor/SOM_GENE_BEG33/data/082725_NovaSeq-X_A_lane2/Trex_c_sedi_S12_L002_R1_001.fastq.gz
-#/mnt/vstor/SOM_GENE_BEG33/data/082725_NovaSeq-X_A_lane2/Trex_ExtrBlank_S14_L002_R1_001.fastq.gz
-#/mnt/vstor/SOM_GENE_BEG33/data/082725_NovaSeq-X_A_lane2/Trex_vessels_S11_L002_R1_001.fastq.gz
-#/mnt/vstor/SOM_GENE_BEG33/data/082725_NovaSeq-X_A_lane2/Trex_v_sedi_S13_L002_R1_001.fastq.gz
+/mnt/vstor/SOM_GENE_BEG33/data/082725_NovaSeq-X_A_lane2/Trex_c_sedi_S12_L002_R1_001.fastq.gz
+/mnt/vstor/SOM_GENE_BEG33/data/082725_NovaSeq-X_A_lane2/Trex_ExtrBlank_S14_L002_R1_001.fastq.gz
+/mnt/vstor/SOM_GENE_BEG33/data/082725_NovaSeq-X_A_lane2/Trex_vessels_S11_L002_R1_001.fastq.gz
+/mnt/vstor/SOM_GENE_BEG33/data/082725_NovaSeq-X_A_lane2/Trex_v_sedi_S13_L002_R1_001.fastq.gz
 
 )
 
--- 02-run-centrifuge.sh
+++ 02-run-centrifuge.sh
@@ -1,5 +1,17 @@
-x=
-centrifuge 
-   centrifuge -p 8 -x $x -U $i,${i/_R1/_R2} --report-file $o.tsv  > $o.txt
-        "
-}
+input=(
+/mnt/vstor/SOM_GENE_BEG33/fossil-c/bigdata/leehom/Trex_cells.fq.gz
+)
+x=/mnt/vstor/SOM_GENE_BEG33/fossil-c/bigdata/centrifuge/hpvc
+
+for f in ${input[@]};do
+	n=${f##*/};n=${n%.fq.gz};
+	o=/mnt/vstor/SOM_GENE_BEG33/fossil-c/bigdata/centrifuge/$n
+	mkdir -p ${o%/*}
+	echo "#!/bin/bash
+		mamba activate dino_env
+	   	centrifuge -p 16 -x $x -U $f --report-file $o.tsv  > $o.txt
+		#awk '\$3==\"U\" {print \$1}' $o.txt > unclassified_reads_ids.txt
+		#seqtk subseq Trex_cells_S10_L002_R1_001.fastq.gz unclassified_reads_ids.txt > R1.filtered.fastq
+
+        " #| sbatch --mem=96g -c 24
+done 
