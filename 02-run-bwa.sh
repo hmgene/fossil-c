d0c2d5e89e36fd0ad31a2a389f957628ec412c79
hmgene <human.gim@gmail.com>
2025-09-17 15:40:32 -0400

    1

--- 02-run-bwa.sh
+++ 02-run-bwa.sh
@@ -11,26 +11,46 @@ input=(
 	bigdata/leehom/Trex_vessels.fq.gz
 	bigdata/leehom/Trex_v_sedi.fq.gz
 )
-odir=bigdata/bwa/results; mkdir -p $odir
+odir=`realpath bigdata/bwa/results`; mkdir -p $odir
 idx=(
-	bigdata/bwa/idx/allMis1.fa
-	bigdata/bwa/idx/anoCar2.fa
+	#bigdata/bwa/idx/allMis1.fa
+	#bigdata/bwa/idx/anoCar2.fa
 	bigdata/bwa/idx/galGal6.fa
-	bigdata/bwa/idx/hg38.fa
+	#bigdata/bwa/idx/hg38.fa
 )
+
+gatk=bigdata/gatk/gatk-4.6.2.0/gatk
 
 for i in ${input[@]};do 
 for j in ${idx[@]};do
+	i=`realpath $i`
+	j=`realpath $j`
 	s=${i##*/};s=${s%.fq.gz*};
 	r=${j##*/};r=${r%.fa*};
 	o=$odir/$s@$r;
 	echo "#!/bin/bash -l
-	mamba activate dyno_env
-	bwa aln $j $i -n 0.01 -l 1000 -o 2 > $o.sai
-	bwa samse $j $o.sai $i -f $o.sam
-	samtools view -F0x4 -hb $o.sam | samtools sort - -@ 16 -T $o > $o.bam
-	samtools index $o.bam
-	rm $o.sam
-	" | sbatch --mem=64g -c 24 
+	mamba activate dino_env
+
+	#bwa aln $j $i -t 24 -n 0.01 -l 1000 -o 2 > $o.sai
+	#bwa samse $j $o.sai $i -f $o.sam
+	#samtools view -F0x4 -hb $o.sam | samtools sort - -@ 16 -T $o > $o.bam
+	fo gatk-run $o $o.bam $j 16
+	#java -Xmx32g -jar bigdata/picard.jar MarkDuplicates \
+	#    I=${o}.bam  O=${o}.DR.bam  M=${o}.metrics.txt  REMOVE_DUPLICATES=True 
+	#samtools index -@ 8 ${o}.DR.bam
+	#samtools view -H ${o}.DR.bam > ${o}.DR.bam.h 
+	#echo -e \"@RG\tID:$s\tSM:$s\tPL:ILLUMINA\" >> ${o}.DR.bam.h
+	#samtools reheader ${o}.DR.bam.h ${o}.DR.bam > ${o}.DR.h.bam 
+	#samtools index ${o}.DR.h.bam
+	#$gatk HaplotypeCaller -R $j -I ${o}.DR.h.bam -O $o.g.vcf.gz -ERC GVCF --sample-name $s
+
+
+	#java -Xmx32g -jar $gatk -T RealignerTargetCreator  -R $j  -I ${o}.DR.bam  -o ${o}.DR.intervals -nt 8
+	#java -Xmx32g -jar $gatk -T IndelRealigner  -R $j  -I ${o}.DR.bam  -targetIntervals ${o}.DR.intervals  -o ${o}.final.bam  --filter_bases_not_stored -nt 8
+	#samtools index -@ 8 ${o}.final.bam
+
+	#rm $o.sam
+	" | sbatch --mem=64g -c 24  --time=100:00:00
+	exit
 done
 done
---
+++ src/gatk.sh
@@ -0,0 +1,51 @@
+
+gatk=bigdata/gatk/gatk-4.6.2.0/gatk
+gatk(){
+	$gatk $@
+}
+gatk-dn(){
+	if [ ! -s $gatk ]; then 
+		o=${gatk%/*}
+		mkdir -p $o;cd $o
+		wget https://github.com/broadinstitute/gatk/releases/download/4.6.2.0/gatk-4.6.2.0.zip
+		unzip gatk-4.6.2.0.zip
+	fi
+}
+gatk-run(){
+usage="$FUNCNAME <prefix/sample_name> <bam> <ref.fa> [thread=8]"
+if [ $# -lt 3 ];then echo "$usage";return;fi
+
+# e.g., output/Brachy_Blank
+sample=${1##*/}; # Bracky_Blank
+odir=${1%/*};   # output 
+mkdir -p $odir
+input_bam=$2;   # e.g., Brachy_Blank@galGal6.DR.bam
+ref=$3;         # e.g., galGal6.fa
+threads=${4:-8};
+
+# Output naming
+dedup_bam="$odir/${sample}.dedup.bam"
+rg_bam="$odir/${sample}.dedup.rg.bam"
+final_bam="$odir/${sample}.final.bam"
+gvcf="$odir/${sample}.g.vcf.gz"
+
+echo "Step 1: Mark Duplicates"
+gatk --java-options "-Xmx32G" MarkDuplicates -I "$input_bam"  -O "$dedup_bam" \
+    -M "${sample}.dedup.metrics.txt" \
+    --REMOVE_DUPLICATES true
+
+echo "Step 2: Add or Replace Read Groups"
+gatk --java-options "-Xmx16G" AddOrReplaceReadGroups  -I "$dedup_bam"  -O "$rg_bam" \
+    -RGID "$sample"  -RGLB "lib1"  -RGPL "illumina"  -RGPU "unit1"  -RGSM "$sample"
+
+echo "Step 3: Index BAM"
+samtools index "$rg_bam"
+
+echo "Step 4: HaplotypeCaller (GVCF mode)"
+gatk --java-options "-Xmx32G" HaplotypeCaller -R "$ref"  -I "$rg_bam"  -O "$gvcf" \
+    -ERC GVCF  --sample-name "$sample" --native-pair-hmm-threads "$threads" \
+    --bam-output $final_bam
+samtools index $final_bam
+
+echo "Done! Output GVCF: $gvcf"
+}
