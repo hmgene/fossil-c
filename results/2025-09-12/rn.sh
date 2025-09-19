c5a0622ca350f1315fe16ddea5519cad9aa146a0
hmgene <human.gim@gmail.com>
2025-09-19 11:24:08 -0400

    up

--- results/2025-09-12/rn.sh
+++ results/2025-09-12/rn.sh
@@ -1,5 +1,10 @@
-Rscript -e 'library(rmarkdown);render("README.Rmd")'
-git add -A
-git commit -am "up"
-git push
+
+if [ -n `find ../../ -size +30M -print -quit`];then
+	git add -A
+	git commit -am "up"
+	git push
+else
+	echo "remove big files:"
+	find ../../ -size +30M
+fi
 
--- src/gatk.sh
+++ src/gatk.sh
@@ -12,40 +12,40 @@ gatk-dn(){
 	fi
 }
 gatk-run(){
-usage="$FUNCNAME <prefix/sample_name> <bam> <ref.fa> [thread=8]"
-if [ $# -lt 3 ];then echo "$usage";return;fi
+	usage="$FUNCNAME <prefix/sample_name> <bam> <ref.fa> [thread=8]"
+	if [ $# -lt 3 ];then echo "$usage";return;fi
 
-# e.g., output/Brachy_Blank
-sample=${1##*/}; # Bracky_Blank
-odir=${1%/*};   # output 
-mkdir -p $odir
-input_bam=$2;   # e.g., Brachy_Blank@galGal6.DR.bam
-ref=$3;         # e.g., galGal6.fa
-threads=${4:-8};
+	# e.g., output/Brachy_Blank
+	sample=${1##*/}; # Bracky_Blank
+	odir=${1%/*};   # output 
+	mkdir -p $odir
+	input_bam=$2;   # e.g., Brachy_Blank@galGal6.DR.bam
+	ref=$3;         # e.g., galGal6.fa
+	threads=${4:-8};
 
-# Output naming
-dedup_bam="$odir/${sample}.dedup.bam"
-rg_bam="$odir/${sample}.dedup.rg.bam"
-final_bam="$odir/${sample}.final.bam"
-gvcf="$odir/${sample}.g.vcf.gz"
+	# Output naming
+	dedup_bam="$odir/${sample}.dedup.bam"
+	rg_bam="$odir/${sample}.dedup.rg.bam"
+	final_bam="$odir/${sample}.final.bam"
+	gvcf="$odir/${sample}.g.vcf.gz"
 
-echo "Step 1: Mark Duplicates"
-gatk --java-options "-Xmx32G" MarkDuplicates -I "$input_bam"  -O "$dedup_bam" \
-    -M "${sample}.dedup.metrics.txt" \
-    --REMOVE_DUPLICATES true
+	echo "Step 1: Mark Duplicates"
+	gatk --java-options "-Xmx32G" MarkDuplicates -I "$input_bam"  -O "$dedup_bam" \
+	    -M "$odir/${sample}.dedup.metrics.txt" --REMOVE_DUPLICATES true
 
-echo "Step 2: Add or Replace Read Groups"
-gatk --java-options "-Xmx16G" AddOrReplaceReadGroups  -I "$dedup_bam"  -O "$rg_bam" \
-    -RGID "$sample"  -RGLB "lib1"  -RGPL "illumina"  -RGPU "unit1"  -RGSM "$sample"
+	echo "Step 2: Add or Replace Read Groups"
+	gatk --java-options "-Xmx16G" AddOrReplaceReadGroups  -I "$dedup_bam"  -O "$rg_bam" \
+	    -RGID "$sample"  -RGLB "lib1"  -RGPL "illumina"  -RGPU "unit1"  -RGSM "$sample"
 
-echo "Step 3: Index BAM"
-samtools index "$rg_bam"
+	echo "Step 3: Index BAM"
+	samtools index "$rg_bam"
 
-echo "Step 4: HaplotypeCaller (GVCF mode)"
-gatk --java-options "-Xmx32G" HaplotypeCaller -R "$ref"  -I "$rg_bam"  -O "$gvcf" \
-    -ERC GVCF  --sample-name "$sample" --native-pair-hmm-threads "$threads" \
-    --bam-output $final_bam
-samtools index $final_bam
+	echo "Step 4: HaplotypeCaller (GVCF mode)"
+	gatk --java-options "-Xmx32G" HaplotypeCaller -R "$ref"  -I "$rg_bam"  -O "$gvcf" \
+	    -ERC GVCF  --sample-name "$sample" --native-pair-hmm-threads "$threads" \
+	    --bam-output $final_bam
+	samtools index $final_bam
 
-echo "Done! Output GVCF: $gvcf"
+	echo "Done! Output GVCF: $gvcf"
+	echo "Done! Output BAM: $final_bam"
 }
