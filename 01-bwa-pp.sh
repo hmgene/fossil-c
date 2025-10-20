8d366af028df1c1a06f5ca1da2262c8ec90b5ebf
hmgene <human.gim@gmail.com>
2025-10-20 11:27:46 -0400

    1

--- 01-bwa-pp.sh
+++ 01-bwa-pp.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 input=(
-#	bigdata/ucsc/fa/allMis1.fa
-#	bigdata/ucsc/fa/anoCar2.fa
-#	bigdata/ucsc/fa/galGal6.fa
-#	bigdata/ucsc/fa/hg38.fa
-#    bigdata/ucsc/fa/mm10.fa.gz
+	bigdata/ucsc/fa/allMis1.fa
+	bigdata/ucsc/fa/anoCar2.fa
+	bigdata/ucsc/fa/galGal6.fa
+	bigdata/ucsc/fa/hg38.fa
+    bigdata/ucsc/fa/mm10.fa.gz
     bigdata/ucsc/fa/loxAfr3.fa.gz
-#    bigdata/genome/bearded_dragon.fna.gz
-#    bigdata/genome/brown_anole.fna.gz
-#    bigdata/genome/crocodile.fna.gz
-#    bigdata/genome/falcon.fna.gz
-#    bigdata/genome/komodo_dragon.fna.gz
-#    bigdata/genome/ostrich.fna.gz
+    bigdata/genome/bearded_dragon.fna.gz
+    bigdata/genome/brown_anole.fna.gz
+    bigdata/genome/crocodile.fna.gz
+    bigdata/genome/falcon.fna.gz
+    bigdata/genome/komodo_dragon.fna.gz
+    bigdata/genome/ostrich.fna.gz
 )
 
 fn(){
