6a4b9bb27bd33a98108535c71db6c1d5f1b8e188
hmgene <human.gim@gmail.com>
2025-11-12 10:53:11 -0500

    1

--- 04-kr2-pl.sh
+++ 04-kr2-pl.sh
@@ -1,8 +1,8 @@
 input=( 
-#brachy_blank_k2_nt,bigdata/kr2/results/Brachy_Blank.k2_report.txt 
-#bracky_cells_k2_nt,bigdata/kr2/results/Brachy_cells.k2_report.txt 
-#bracky_blank_cf_nt,bigdata/centrifuge/results/Brachy_Blank.krreport.txt 
-#bracky_cells_cf_nt,bigdata/centrifuge/results/Brachy_cells.krreport.txt 
+brachy_blank_k2_nt,bigdata/kr2/results/Brachy_Blank.k2_report.txt 
+bracky_cells_k2_nt,bigdata/kr2/results/Brachy_cells.k2_report.txt 
+bracky_blank_cf_nt,bigdata/centrifuge/results/Brachy_Blank.krreport.txt 
+bracky_cells_cf_nt,bigdata/centrifuge/results/Brachy_cells.krreport.txt 
 bigdata/centrifuge/results/BC_SRSLY.krreport.txt 
 )
 odir=results/2025-11-05;mkdir -p $odir
@@ -17,9 +17,10 @@ for i in ${input[@]};do
 n=${i%,*}
 f=${i#*,}
 o=$odir/$n
-[ -s $o.html ] || \
+#[ -s $o.html ] || \
 echo "#!/bin/bash
-python ../KrakenTools/kreport2krona.py -r $f -o $o.krona.txt
-ktImportText $o.krona.txt  -o $o.krona.html 
+#python ../KrakenTools/kreport2krona.py -r $f -o $o.krona.txt
+python ../KrakenTools/filter_bracken_kreport.py -r $o.krona.txt -t 0.01 -o $o.krona.flt.txt 
+ktImportText $o.krona.flt.txt  -o $o.krona.html 
 " | sbatch  -o $o.out 
 done
