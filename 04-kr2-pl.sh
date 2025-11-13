7d18089dae31cf18381c89a708aafd073bb8016c
hmgene <human.gim@gmail.com>
2025-11-13 14:15:27 -0500

    1

--- 04-kr2-pl.sh
+++ 04-kr2-pl.sh
@@ -1,9 +1,9 @@
 input=( 
-brachy_blank_k2_nt,bigdata/kr2/results/Brachy_Blank.k2_report.txt 
-bracky_cells_k2_nt,bigdata/kr2/results/Brachy_cells.k2_report.txt 
-bracky_blank_cf_nt,bigdata/centrifuge/results/Brachy_Blank.krreport.txt 
+#brachy_blank_k2_nt,bigdata/kr2/results/Brachy_Blank.k2_report.txt 
+#bracky_cells_k2_nt,bigdata/kr2/results/Brachy_cells.k2_report.txt 
+#bracky_blank_cf_nt,bigdata/centrifuge/results/Brachy_Blank.krreport.txt 
 bracky_cells_cf_nt,bigdata/centrifuge/results/Brachy_cells.krreport.txt 
-BC_SRSLY,bigdata/centrifuge/results/BC_SRSLY.krreport.txt 
+#BC_SRSLY,bigdata/centrifuge/results/BC_SRSLY.krreport.txt 
 )
 odir=results/2025-11-05;mkdir -p $odir
 
@@ -22,5 +22,5 @@ echo "#!/bin/bash
 dino kr2-filter $f 1 > $o.kreport 
 python ../KrakenTools/kreport2krona.py -r $o.kreport -o $o.krona.txt --intermediate-ranks
 ktImportText $o.krona.txt  -o $o.krona.html 
-" | sbatch  -o $o.out 
+" #| sbatch  -o $o.out 
 done
