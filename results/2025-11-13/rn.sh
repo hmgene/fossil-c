47e58e421a81b1d2df1165faf5cf2ba1f3a614f1
hmgene <human.gim@gmail.com>
2025-11-13 10:36:55 -0500

    md-render

--- results/2025-11-13/rn.sh
+++ results/2025-11-13/rn.sh
@@ -20,30 +20,29 @@ for (grp in names(groups)) {
     dt <- fread(tsv)
     score_cols <- setdiff(names(dt), c("id", "seq"))
     if (length(score_cols) == 0) next
-    dt_best <- dt[, .( best_score = do.call(pmax, .SD), best_ref   = score_cols[max.col(.SD, ties.method = "first")]), .SDcols = score_cols]
+    dt_best <- dt[, .( best_score = do.call(pmax, .SD), 
+        best_ref   = score_cols[max.col(.SD, ties.method = "first")]), .SDcols = score_cols]
     dt_best[, read_len:=nchar(dt[,seq])]
     dt_best[, prop := best_score / read_len]
     dt_best[, ref := sub(".*@", "", best_ref)]
+    dt_best[, ref_label := paste0(best_ref, " (n=", .N, ")"), by = best_ref]
+
 
     library(ggplot2)
     library(dplyr)
     library(tidyr)  # needed for unnest
 
-    dt_norm <- dt_best %>% group_by(best_ref) %>%
+    dt_norm <- dt_best %>% group_by(ref) %>%
       summarise( dens = list(density(best_score / read_len, adjust = 1.2)), .groups = "drop") %>%
       mutate( x = lapply(dens, function(d) d$x), y = lapply(dens, function(d) d$y / max(d$y))) %>%
-      select(best_ref, x, y) %>% unnest(cols = c(x, y))  # tidyr unnest
+      select(ref, x, y) %>% unnest(cols = c(x, y))  # tidyr unnest
 
     # Plot
-    p <- ggplot(dt_norm, aes(x = x, y = y, color = best_ref)) +
+    p <- ggplot(dt_norm, aes(x = x, y = y, color = ref_label)) +
       geom_line(size = 1) + labs( x = "Best Score / Read Length", y = "Density (normalized by max)",
-        title = "Alignment Quality per Reference (Peak Normalized)"
-      ) +
+        title = "Alignment Quality per Reference (Peak Normalized)") +
       theme_minimal(base_size = 6) +
-      theme(
-        plot.title = element_text(hjust = 0.5),
-        legend.title = element_blank()
-      )
+      theme( plot.title = element_text(hjust = 0.5), legend.title = element_blank())
     if (!dir.exists("figures")) dir.create("figures", recursive = TRUE)
     ggsave(paste0("figures/alignment_proportion_",sample_name,".png"), plot = p, width = 6, height = 4, dpi = 300)
 
