a38071152734747f475a3773b48f27b080c61466
hmgene <human.gim@gmail.com>
2025-12-09 11:34:27 -0500

    md-render

--- results/2025-11-05/rn.sh
+++ results/2025-11-05/rn.sh
@@ -1,15 +1,18 @@
 #for f in ../../bigdata/centrifuge/results/*.krreport.txt;do
 #    n=${f##*/};n=${n%.krreport*};
-#    cat $f | awk '$4=="S"' | sort -nrk 3 | head -n 5 | awk -v OFS="\t" -v n=$n '{print n,$6;}' 
+#    {
+#        head -2 $f 
+#        cat $f | awk '$4=="S"' | sort -nrk 3 | head -n 5 
+#    } | awk -v OFS="\t" -v n=$n '{print n,$2,$6;}' 
 #done > tt
-
 cat tt | perl -e 'use strict;
     my %r=();
-    while(<>){chomp;my($n,$v)=split/\s+/,$_;
-        push @{$r{$n}},$v;
+    while(<>){chomp;my($n,$c,$v)=split/\s+/,$_;
+        $r{$n}{$c}=$v;
     }
-    print join("\t","sample",map{ "top$_" }  1..5 ),"\n";
+    print join("\t","sample",map{ "top$_" }  1..7 ),"\n";
     foreach my $n (keys %r){
-        print join("\t",$n,@{$r{$n}}),"\n";
+        my @x = sort {$b<=>$a} keys %{$r{$n}};
+        print join("\t",$n, map { "$r{$n}{$_} ( $_ )" } @x),"\n";
     }
 ' > summ.tsv
