e305570ae2731ef9aa0459ca80ae24e8f075f8e3
hmgene <human.gim@gmail.com>
2026-01-12 10:58:27 -0500

    md-render

--- tmp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-tail -n+2 bigdata/bwa_scores_+contig/Brachy_cells.tsv | cut -f 1 | uniq | wc -l > o
-tail -n+2 bigdata/bwa_scores_+contig/Brachy_vessels.tsv | cut -f 1 | unique | wc -l >> o
