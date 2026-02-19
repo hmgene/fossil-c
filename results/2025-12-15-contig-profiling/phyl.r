c1d5d0918256847ec182db32a0f175a2df251c5f
hmgene <human.gim@gmail.com>
2026-02-19 10:32:15 -0500

    1

--- results/2025-12-15-contig-profiling/phyl.r
+++ results/2025-12-15-contig-profiling/phyl.r
@@ -4,7 +4,8 @@ library(cowplot)
 library(tools)
 input="../../bigdata/bwa_scores_+contig/"
 #tsv_files <- list.files(input, pattern = "Brachy_(Blank|cells).*\\.tsv$", full.names = TRUE, recursive = F)
-tsv_files <- list.files(input, pattern = "Brachy_(vessels).*\\.tsv$", full.names = TRUE, recursive = F)
+#tsv_files <- list.files(input, pattern = "Brachy_(vessels).*\\.tsv$", full.names = TRUE, recursive = F)
+tsv_files <- list.files(input, pattern = "Trex_(cells|vessels).*\\.tsv$", full.names = TRUE, recursive = F)
 pattern_order <- c("cells", "vessels", "c_sedi", "v_sedi", "blank")
 tsv_files<- tsv_files[order(sapply(tsv_files, function(x) {
   x_lower <- tolower(basename(x))
@@ -32,7 +33,7 @@ for (grp in names(groups)) {
     x=dt_best[prop>=0.9]
     x[, ref_label := paste0(ref, " (n=", .N, ")"), by = best_ref]
 
-    fwrite(dt_best,file=paste0("data/",sample_name,".csv.gz"))
+    fwrite(dt_best,file=paste0("bigdata/",sample_name,".csv.gz")) ## bigdata
     library(ggplot2)
     library(dplyr)
     library(tidyr)  # needed for unnest
@@ -41,7 +42,7 @@ for (grp in names(groups)) {
       summarise( dens = list(density(best_score / read_len, adjust = 1.2)), .groups = "drop") %>%
       mutate( x = lapply(dens, function(d) d$x), y = lapply(dens, function(d) d$y )) %>%
       select(ref_label, x, y) %>% unnest(cols = c(x, y))  # tidyr unnest
-    fwrite(dt,file=paste0("data/alignment_proportion_",sample_name,".csv.gz"))
+    fwrite(dt,file=paste0("data/alignment_proportion_",sample_name,".csv.gz")) 
 
 
     p <- ggplot(dt, aes(x = x, y = y, color = ref_label)) +
